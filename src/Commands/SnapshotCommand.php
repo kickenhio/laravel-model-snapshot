1f69f4cd205bc9c0566147b3661061c5c3eabf50
kickenhio <kickenhio@gmail.com>
2022-12-30 14:26:34 +0100

    Command embedded

---
+++ src/Commands/SnapshotCommand.php
@@ -0,0 +1,46 @@
+<?php namespace Kickenhio\LaravelSqlSnapshot\Commands;
+
+use Illuminate\Console\Command;
+use Kickenhio\LaravelSqlSnapshot\Exceptions\InvalidManifestSyntaxException;
+use Symfony\Component\Console\Command\Command as CommandAlias;
+use Kickenhio\LaravelSqlSnapshot\Facades\Snapshot as SnapshotSQL;
+
+class SnapshotCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sql-snapshot {manifest} {model} {id*}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Retrieves SQL queries to replicate DB entries';
+
+    /**
+     * @return int
+     */
+    public function handle(): int
+    {
+        try {
+            $snapshot = SnapshotSQL::fromManifest($this->argument('manifest'));
+
+            foreach ($this->argument('id', []) as $id) {
+                foreach ($snapshot->retrieve($this->argument('model'), $id)->toSql() as $query) {
+                    if (substr($query, 0, 2) !== '--') {
+                        $this->getOutput()->text($query . PHP_EOL);
+                    }
+                };
+            }
+
+        } catch (InvalidManifestSyntaxException $e) {
+            $this->output->error($e);
+        }
+
+        return CommandAlias::SUCCESS;
+    }
+}
--- src/SnapshotServiceProvider.php
+++ src/SnapshotServiceProvider.php
@@ -1,6 +1,7 @@
 <?php namespace Kickenhio\LaravelSqlSnapshot;
 
 use Illuminate\Support\ServiceProvider;
+use Kickenhio\LaravelSqlSnapshot\Commands\SnapshotCommand;
 
 class SnapshotServiceProvider extends ServiceProvider
 {
@@ -33,6 +34,7 @@ class SnapshotServiceProvider extends ServiceProvider
         if ($this->app->runningInConsole()) {
             $this->publishes([ $this->getConfigFile() => config_path('snapshot.php') ], 'config');
             $this->publishes([ $this->getManifestFile() => resource_path('example.json') ], 'manifest');
+            $this->commands([SnapshotCommand::class]);
         } else {
             $this->loadRoutesFrom(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'routes' . DIRECTORY_SEPARATOR . 'routes.php');
         }
